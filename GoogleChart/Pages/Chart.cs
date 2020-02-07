a7acf1af1506eb64c42c2511e9faaf4339286d18
zoltanhalasz <zoltan.halasz@gmail.com>
2020-02-07 16:57:50 +0200

    Add project files.

---
+++ GoogleChart/Pages/Chart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoogleChart.Pages
+{
+    public class Chart
+    {
+        public object[] cols { get; set; }
+        public object[] rows { get; set; }
+    }
+}
---
+++ GoogleChart/Pages/Index.cshtml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.DataTable.Net.Wrapper;
+using Google.DataTable.Net.Wrapper.Extension;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace GoogleChart.Pages
+{
+
+
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public ActionResult OnGetChartData()
+        {
+            Random rnd = new Random();
+            var pizza = new[]
+                {
+                new {Name = "Mushrooms", Count = rnd.Next(1, 13)},
+                new {Name = "Onions", Count = rnd.Next(1, 13)},
+                new {Name = "Olives", Count = rnd.Next(1, 13)},
+                new {Name = "Zucchini", Count = rnd.Next(1, 13)},
+                new {Name = "Pepperoni", Count = rnd.Next(1, 13)}
+            };
+
+            var json = pizza.ToGoogleDataTable()
+                    .NewColumn(new Column(ColumnType.String, "Topping"), x => x.Name)
+                    .NewColumn(new Column(ColumnType.Number, "Slices"), x => x.Count)
+                    .Build()
+                    .GetJson();
+
+            return Content(json);
+        }
+    }
+}
